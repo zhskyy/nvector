bdb601710bc8a0074dbbd10f44027ee221ae702c
Vitaliy Zhaborovskiy <vitaliyz@similarweb.com>
2019-05-13 16:00:23 +0300

    add first tests and template for implementation

--- src/nvector.hpp
+++ src/nvector.hpp
@@ -1,4 +1,51 @@
+#include <cassert>
+#include <string>
+#include <stdexcept>
+
+template <class Type>
 class nvector {
-	nvector(){}
-	~nvector(){}
+    //replace all assert(0); for implementations
+public:
+    nvector()
+    {
+        assert(0);//replace all assert(0); for implementations
+    }
+
+    virtual ~nvector()
+    {
+        assert(0);
+    }
+
+    nvector(const nvector& op)
+    {
+        assert(0);
+    }
+
+    nvector(nvector&& op)
+    {
+        assert(0);
+    }
+
+    nvector& operator=(const nvector& op) const
+    {
+        assert(0);
+    }
+
+    Type operator[](const long i) const
+    {
+        assert(0);
+    }
+
+    Type& operator[](const long i)
+    {
+        assert(0);
+    }
+
+    Type at(const long i) const
+    {
+        throw std::range_error("out of range at function at");
+    }
+
+private:
+    Type* __data;
 };
--- tests/nvector_tests.cpp
+++ tests/nvector_tests.cpp
@@ -5,3 +5,10 @@ TEST(base_test, chacking_test)
 {
 	ASSERT_EQ(0, 0) << "!";
 }
+
+TEST(nvector, constructor_test)
+{
+    EXPECT_NO_FATAL_FAILURE({
+        nvector<double> x = nvector<double>();
+    });
+}
