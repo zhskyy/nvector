2df09f7dde1fa6993a9d621ed65909c724d02187
Vitaliy Zhaborovskiy <vitaliyz@similarweb.com>
2019-06-17 20:38:16 +0300

    tests for operator= and operator[] added

--- tests/nvector_tests.cpp
+++ tests/nvector_tests.cpp
@@ -14,3 +14,25 @@ TEST(nvector, constructor_test)
         nvector<nvector<int>> yy = nvector<nvector<int>>();
     });
 }
+
+TEST(nvector, operator_eq_test_1)
+{
+    nvector<double> x1(5), x2(3);
+    x1 = x2;
+
+    //EXPECT_THROW(x1.at(4), std::range_error);
+    EXPECT_EQ(x1.size(), 3);
+
+}
+
+TEST(nvector, operator_squre_bars_1)
+{
+    struct my_own_type {
+      int x, y;
+    };
+
+    const nvector<my_own_type> x(2);
+
+    const my_own_type expected_result = x[7L];
+    EXPECT_EQ(expected_result, my_own_type());
+}
